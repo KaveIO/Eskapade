434e2e96b29227f9cc5c43a87d1ab4e715899b22
Max Baak <Baak.Max@kpmg.nl>
2017-03-31 12:21:41 +0200

    Remove obsolete link histogrammar_summary.py

--- python/eskapade/visualization/links/__init__.py
+++ python/eskapade/visualization/links/__init__.py
@@ -2,4 +2,3 @@ __all__ = ['DfSummary','HistSummary','HistogrammarSummary']
 from .df_summary import DfSummary
 from .hist_summary import HistSummary
 from .histogrammar_summary import HistogrammarSummary
-
