2ace3f96255c5e1aa52768752bbb00858848fd35
Max Baak <Baak.Max@kpmg.nl>
2017-04-04 22:31:52 +0200

    Added option to ds_object_deleter link to rm objects of certain classes

--- python/eskapade/core_ops/links/ds_object_deleter.py
+++ python/eskapade/core_ops/links/ds_object_deleter.py
@@ -26,15 +26,17 @@ class DsObjectDeleter(Link):
         Store the configuration of link DsObjectDeleter
 
         :param str name: name of link
-        :param lst deletionKeys: keys to clear. Overwrites clearAll to false.
-        :param lst keepOnly: keys to keep. Overwrites clearAll to false.
+        :param list deletionKeys: keys to clear. Overwrites clearAll to false.
+        :param list deletionClasses: delete object(s) by class type.
+        :param lsst keepOnly: keys to keep. Overwrites clearAll to false.
         :param bool clearAll: clear all key-value pairs in the datastore. Default is true.
         """
         
         Link.__init__(self, kwargs.pop('name', 'DsObjectDeleter'))
 
         # process keyword arguments
-        self._process_kwargs(kwargs, deletionKeys=[], keepOnly=[], clearAll=True)
+        self._process_kwargs(kwargs, deletionKeys=[], deletionClasses=[],
+                             keepOnly=[], clearAll=True)
         self.check_extra_kwargs(kwargs)
         
         return
@@ -44,6 +46,7 @@ class DsObjectDeleter(Link):
 
         # Overwrites clearAll to false if individual keys are set.
         if len(self.deletionKeys): self.clearAll = False
+        if len(self.deletionClasses): self.clearAll = False
         if len(self.keepOnly): self.clearAll = False
         
         return StatusCode.Success
@@ -64,6 +67,13 @@ class DsObjectDeleter(Link):
             if key in ds:
                 self.log().debug('Now deleting datastore object with key <%s>', key)
                 del ds[key]
+
+        # delete specific class types
+        for cls in self.deletionClasses:
+            for key in ds:
+                if isinstance(ds[key],cls):
+                    self.log().debug('Now deleting datastore object with key <%s>', key)
+                    del ds[key]
 
         # delete all but specific items
         if len(self.keepOnly):
