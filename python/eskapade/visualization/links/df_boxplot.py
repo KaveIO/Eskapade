6a7fb819da76e8b0f7483b043eec0cd985324fff
Lodewijk Nauta <nauta.lodewijk@kpmg.nl>
2017-04-21 15:00:00 +0200

    Add boxplot vis_utils and link
    The vis_utils now contains box_plot function and delete_smallstat
    functions that create the both the plot of a column that is
    grouped by another column and also delete the low-statistics
    values before they are added to the plot.

---
+++ python/eskapade/visualization/links/df_boxplot.py
@@ -0,0 +1,176 @@
+# **********************************************************************************
+# * Project: Eskapade - A python-based package for data analysis                   *
+# * Class  : DfBoxplot                                                             *
+# * Created: 2017/02/17                                                            *
+# *                                                                                *
+# * Description:                                                                   *
+# *      Link to create a boxplot of data frame columns                            *
+# *                                                                                *
+# * Authors:                                                                       *
+# *      KPMG Big Data team, Amstelveen, The Netherlands                           *
+# *                                                                                *
+# * Redistribution and use in source and binary forms, with or without             *
+# * modification, are permitted according to the terms listed in the file          *
+# * LICENSE.                                                                       *
+# **********************************************************************************
+
+import os
+import pandas as pd
+import numpy
+
+from eskapade import StatusCode, DataStore, Link, ProcessManager, ConfigObject
+from eskapade import core, visualization
+from eskapade.analysis import statistics
+
+NUMBER_OF_RECORDS = 1000
+
+
+class DfBoxplot(Link):
+    """Create a boxplot of one column of a DataFrame that is grouped by values from a second column.
+
+    Creates a report page for each variable in DataFrame, containing:
+
+    * a profile of the column dataset
+    * a nicely scaled plot of the boxplots per group of the column
+
+    Example is available in: tutorials/esk304_df_boxplot.py
+    """
+
+    def __init__(self, **kwargs):
+        """Initialize the DfSummary link
+
+        :param str name: name of link
+        :param str read_key: key of input data to read from data store
+        :param str results_path: output path of summary result files
+        :param str column: column pick up from input data to use as boxplot input
+        :param list cause_columns: list of columns (str) to group-by, and per unique value plot a boxplot
+        """
+
+        # initialize Link
+        Link.__init__(self, kwargs.pop('name', 'df_boxplot'))
+
+        # process keyword arguments
+        self._process_kwargs(kwargs, read_key='', results_path='', column=None, cause_columns=None)
+        self.check_extra_kwargs(kwargs)
+
+        # initialize attributes
+        self.pages = []
+
+    def initialize(self):
+        """Inititialize DfSummary link"""
+
+        # check input arguments
+        self.check_arg_types(read_key=str)
+        self.check_arg_types(recurse=True, allow_none=True, column=str, cause_columns=list)
+        self.check_arg_vals('read_key')
+
+        # get I/O configuration
+        io_conf = ProcessManager().service(ConfigObject).io_conf()
+
+        # read report templates
+        with open(core.persistence.io_path('templates', io_conf, 'df_summary_report.tex')) as templ_file:
+            self.report_template = templ_file.read()
+        with open(core.persistence.io_path('templates', io_conf, 'df_summary_report_page.tex')) as templ_file:
+            self.page_template = templ_file.read()
+
+        # get path to results directory
+        if not self.results_path:
+            self.results_path = core.persistence.io_path(
+                'results_data', io_conf, 'report')
+
+        # check if output directory exists
+        if os.path.exists(self.results_path):
+            # check if path is a directory
+            if not os.path.isdir(self.results_path):
+                self.log().critical('output path "%s" is not a directory', self.results_path)
+                raise AssertionError('output path is not a directory')
+        else:
+            # create directory
+            self.log().debug('Making output directory %s', self.results_path)
+            os.makedirs(self.results_path)
+
+        return StatusCode.Success
+
+    def execute(self):
+        """Execute DfSummary
+
+        Creates a report page for each group that we group-by in the data frame.
+
+        * create statistics object for group
+        * create overview table of column variable
+        * plot histogram of column variable
+        * store plot
+        """
+
+        # import matplotlib here to prevent import before setting backend in
+        # core.execution.run_eskapade
+        import matplotlib.pyplot as plt
+        from matplotlib.backends.backend_pdf import PdfPages
+
+        # fetch and check input data frame
+        data = ProcessManager().service(DataStore).get(self.read_key, None)
+        if not isinstance(data, pd.DataFrame):
+            self.log().critical('No Pandas data frame "%s" found in data store for %s', self.read_key, str(self))
+            raise RuntimeError('no input data found for %s' % str(self))
+
+        # create report page for each variable in data frame
+        self.pages = []
+        for col in self.cause_columns:
+            # output column name
+            self.log().debug('processing cause column "%s"', col)
+
+            # check if column is in data frame
+            if col not in data.columns:
+                self.log().warning('column "%s" not in data frame', col)
+                continue
+
+            # 1. create statistics object for column
+            var_label = col
+
+            # TODO: Change the statistics that we want to put on the page
+            # stats = statistics.ArrayStats(data, col, unit=self.var_units.get(col, ''), label=var_label)
+
+            # evaluate statistical properties of array
+            # stats.create_stats()
+
+            # make histogram
+            # nphist = stats.make_histogram(var_bins=self.var_bins.get(col, NUMBER_OF_BINS))
+
+            # determine histogram properties for plotting
+            # x_label = stats.get_x_label()
+            # y_label = self.hist_y_label if self.hist_y_label else None
+            # is_num = stats.get_col_props()['is_num']
+            # is_ts = stats.get_col_props()['is_ts']
+
+            # 3. plot histogram of column variable
+            visualization.vis_utils.box_plot(data, col, self.column)
+
+            # 4. store plot
+            box_file_name = 'boxplot_{}.pdf'.format(col)
+            pdf_file = PdfPages(
+                '{0:s}/{1:s}'.format(self.results_path, box_file_name))
+            plt.savefig(
+                pdf_file,
+                format='pdf',
+                bbox_inches='tight',
+                pad_inches=0)
+            plt.close()
+            pdf_file.close()
+
+            # create overview table of column variable
+            # TODO: Make this compatible with new statistics function
+            # stats_table = stats.get_latex_table()
+
+            # create page string
+            self.pages.append(self.page_template.replace('VAR_LABEL', var_label)
+                                                # .replace('VAR_STATS_TABLE', stats_table)
+                                                .replace('VAR_HISTOGRAM_PATH', box_file_name))
+
+        # write report file
+        with open('{}/report_boxplots.tex'.format(self.results_path), 'w') as report_file:
+            report_file.write(
+                self.report_template.replace(
+                    'INPUT_PAGES', ''.join(
+                        self.pages)))
+
+        return StatusCode.Success
--- python/eskapade/visualization/vis_utils.py
+++ python/eskapade/visualization/vis_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import logging
+import matplotlib.pyplot as plt
 
 NUM_NS_DAY = 24 * 3600 * int(1e9)
 
@@ -141,3 +143,108 @@ def plot_2d_histogram(hist, x_lim, y_lim, title, x_label, y_label, pdf_file_name
         plt.savefig(pdf_file, format='pdf', bbox_inches='tight', pad_inches=0)
         plt.close()
         pdf_file.close()
+
+
+def delete_smallstat(df, group_col, statlim=400):
+    """Function to make a new DataFrame that removes all groups of group_col that have less than statlim entries.
+
+    :param df: pandas DataFrame
+    :param str group_col: name of the column to group on
+    :param int statlim: number of entries a group has to have to be statistically significant
+    :returns: smaller DataFrame and the number of removed categories
+    :rtype: tuple
+    """
+    sizes = df.groupby(group_col).size()
+    group_sizes = list(zip(df.groupby(group_col).size().index, sizes))
+
+    # Check if there are groups that are too small, if not, return the original DataFrame
+    number_toosmall = (sizes < statlim).sum()
+    if number_toosmall == 0:
+        return df, 0
+
+    # Define the selections for the groups that are too small
+    i = 0
+    for group, lim in group_sizes:
+        if lim < statlim:
+            if i == 0:
+                selstring = (df[group_col] != group)
+            else:
+                # Kept this line explicit for clarity
+                selstring = selstring & (df[group_col] != group)
+            i += 1
+
+    # Return the smaller DataFrame
+    df_small = df[selstring]
+    return df_small, i
+
+
+def box_plot(df, cause_col, result_col='cost', ylim_quant=0.95, ylim_high=None, ylim_low=0, rot=90, statlim=400,
+        label_dict=None, title_add=''):
+    """ Function that plots the boxplot of the column df[result_col] in groups of cause_col. This means that
+    the DataFrame is grouped-by on the cause column and then the distribution per group is plotted in a boxplot
+    using the standard pandas functionality.
+    Boxplots with less than statlim (default=400 ) entries in it are automatically removed.
+
+    Input:
+    :param df: pandas DataFrame, cause column and result column
+    :param str cause_col: name of the column to group on. This can technically be a number, but that is very uncommon.
+    :param str result_col: column to do the boxplot on
+    :param float ylim_quant: the quantile of the y upper limit
+    :param float ylim_high: when defined, this limit is used, when not defined, defaults to None and ylim_high is
+           determined by ylim_quant
+    :param float ylim_low: matplotlib set_ylim lower bound
+    :param int rot: matplotlib rot
+    :param int statlim: the number of entries that a group is required to have in order to be plotted
+    :param dict label_dict: dictionary with labels for the columns, usage example: label_dict={'col_x': 'Time'}
+    :param str title_add: string that is added to the automatic title (the y column name)
+    """
+
+    # Check the number of categories in the cause_col, if this is too large, only plot the top 20.
+    if len(df[cause_col].unique()) > 20:
+        top_20 = df[cause_col].value_counts()[:20].index
+        df = df[df[cause_col].isin(top_20)]
+        logging.debug('The number of categories of column {0} is too large, boxplot is not '
+                      'generated'.format(cause_col))
+
+    fig = plt.figure(figsize=(15, 20))
+    ax1 = fig.add_subplot(211)
+
+    df_small, n_removed = delete_smallstat(df, cause_col, statlim=statlim)
+
+    # Make boxplots
+    df_small.boxplot(column=result_col, by=cause_col, ax=ax1, fontsize=20, rot=rot, grid=True)
+
+    # If columns do not have a pretty name in label_dict, make the label the column name
+    try:
+        xlabel = label_dict[cause_col]
+    except:
+        xlabel = cause_col
+    ax1.set_xlabel(xlabel, fontsize=20)
+
+    try:
+        title_label = label_dict[result_col]+title_add
+    except:
+        title_label = result_col+title_add
+    ax1.set_title(title_label, fontsize=20)
+
+    # Label parameters
+    ax1.tick_params(axis='both', which='major', labelsize=20)
+    subfig = ax1.get_figure()
+    subfig.suptitle('')
+
+    # Calculate quantile for y axis limit
+    if ylim_high is None:
+      ylim_high = df[result_col].dropna().quantile(q=ylim_quant)
+
+    ax1.set_ylim(ylim_low, ylim_high)
+
+    # Put the number of entries in each group as a number above each boxplot
+    num_boxes = len(df_small[cause_col].unique())
+    pos = np.arange(num_boxes) + 1
+    sizes = list(df_small.groupby(cause_col).size())
+    upper_labels = [str(np.round(s, 2)) for s in sizes]
+    weights = ['bold', 'semibold']
+    for tick, label in zip(range(num_boxes), ax1.get_xticklabels()):
+        k = tick % 2
+        ax1.text(pos[tick], ylim_high - (ylim_high*0.05), upper_labels[tick],
+                 horizontalalignment='center', size='larger', weight=weights[k])
