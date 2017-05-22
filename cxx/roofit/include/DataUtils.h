0a634ee320ca100242dde05b704fff01173d23b8
Max Baak <Baak.Max@kpmg.nl>
2017-05-22 13:27:43 +0200

    Roofit code to calculate correlation and residuals of observables
    Added roofit pdf RooABCDHistPdf that can be used to test the factorization
    assumption of observables in arbitrary number-counting datasets, and to
    find the most dominant outliers in this assumptions.
    
    I have committed the following files (that all connect to RooABCDHistPdf):
    
    Classes:
    
    - RooABCDHistPdf.cxx/.h:
    RooFit pdf that can be used to test the factorization
    assumption of observables in arbitrary number-counting datasets, and to
    find the most dominant outliers in this assumptions.
    
    - RooParamHistPdf.cxx/.h
    RooABCDHistPdf inherits from RooParamHistPdf, which is a roofit Pdf class
    derived from RooHistPdf which multiplies each bin with a RooRealVar scale
    parameter, e.g. which can be fit or constrained.
    
    - RooExpandedFitResult.cxx/.h
    A class derived from RooFitResult, to be able to add more parameters
    for error propagation (for calculation & visualization purposes), or to
    build a RooFitResult object from set of parameters
    
    - TMsgLogger.cxx/.h
    An output logger class producing nicely formatted log messages, adapted from
    TMVA::MsgLogger
    
    Modules:
    
    - ABCDUtils.cxx/.h
    A Collection of ROOT utility functions for RooABCDHistPdf. In particular:
    A function that returns the (significance of the) p-value of the
    hypothesis that the observables in the input dataset
    are *not* correlated.
    A function returns a dataset with the normalized residuals
    (= pull values) for all bins of the input data.
    
    - Statistics.cxx/.h
    A collection of ROOT statistics utility functions, based on RooStats and RooFit, namely:
    number counting utilities, giving the corresponding p and Z values, and
    error propagation function.
    
    - DataUtils.cxx/.h
    A collection of ROOT utility functions for processing RooFit RooDataSets:
    to convert a roodatahist into a roodataset, and
    to calculate the error on a given function for each row in a
    RooDataSet, and add this error as a new column to the dataset.

---
+++ cxx/roofit/include/DataUtils.h
@@ -0,0 +1,45 @@
+/*****************************************************************************
+ * Project: Eskapade - A python-based package for data analysis              *
+ * Module : DataUtils                                                        *
+ * Created: 2017/06/03                                                       *
+ * Description:                                                              *
+ *      Collections of utility functions for processing RooFit RooDataSets:
+ *      - convert a roodatahist into a roodataset
+ *      - calculate the error on a given function for each row in a
+ *        RooDataSet, and add this error as a new column to the dataset.
+ *      Both functions are implemented as ROOT functions.                    *
+ *                                                                           *
+ * Authors:                                                                  *
+ *      KPMG Big Data team, Amstelveen, The Netherlands                      *
+ *                                                                           *
+ * Redistribution and use in source and binary forms, with or without        *
+ * modification, are permitted according to the terms listed in the file     *
+ * LICENSE.                                                                  *
+ *****************************************************************************/
+
+#ifndef Eskapade_Data_Utils
+#define Eskapade_Data_Utils
+
+#include "RooDataHist.h"
+#include "RooDataSet.h"
+#include "RooAbsReal.h"
+#include "RooFitResult.h"
+#include "TString.h"
+
+namespace Eskapade {
+
+  // Function to convert a roodatahist into a roodataset.
+  // Required input is the roodatahist (dataHist). Optionally provide a new name (dsName).
+  RooDataSet* ConvertDataHistToDataSet(const RooDataHist& dataHist, const char* dsName=0);
+
+  // Function to calculate the error on a given function for each row in the input
+  // RooDataSet (data), and add this error as a new column to this dataset (with name errName).
+  // obsSet are the observables needed to evaluate the function func.
+  // fitResult is needed to do error propagation on each func value.
+  // If addFuncVal is true, the function values are stored as a new column as well.
+  void AddPropagatedErrorToData(RooDataSet& data, RooArgSet& obsSet, RooAbsReal& func,
+                                const RooFitResult& fitResult,
+                                const char* errName=0, Bool_t addFuncVal=kFALSE);
+}
+
+#endif
