a08c6ee9d7bad9005292ae7fc4f40c62005ace7f
Max Baak <Baak.Max@kpmg.nl>
2018-02-12 11:52:07 +0100

    Docker: option if uid already remapped no need to redo it
    If uid and gui have already been mapped to the correct values, then they are
    not remapped the next time the docker images is booted up.

--- docker/eskapade-usr/user-mapping.sh
+++ docker/eskapade-usr/user-mapping.sh
@@ -10,6 +10,10 @@ if [ -z "${HOST_USER_ID}" -a -z "${HOST_USER_GID}" ]; then
     echo "Both HOST_USERD_ID and HOST_USER_GID environment variables should be set." ; exit 0
 fi
 
+# current user and group id
+CURRENT_UID=`id -u ${USER}`
+CURRENT_GID=`id -g ${USER}`
+
 # reset user_id to either new id or if empty old (still one of above might not be set)
 USER_ID=${HOST_USER_ID:=$USER_ID}
 USER_GID=${HOST_USER_GID:=$USER_GID}
@@ -26,8 +30,10 @@ sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${USER_GID}/"  /etc/group
 
 # chown directory (if docker host/container id's do not match)
 if [[ "${HOST_USER_ID}" -ne 1000 ]] && [[ "${HOST_USER_GID}" -ne 1000 ]]; then
-    echo 'Applying user mapping to set permissions for' ${USER_HOME} '(this may take some time)'
-    chown -R ${USER_ID}:${USER_GID} ${USER_HOME}
+    if [[ "${HOST_USER_ID}" -ne "${CURRENT_UID}" ]] && [[ "${HOST_USER_GID}" -ne "${CURRENT_GID}" ]]; then
+        echo 'Applying user mapping to set permissions for' ${USER_HOME} '(this may take some time)'
+        chown -R ${USER_ID}:${USER_GID} ${USER_HOME}
+    fi
 fi
 
 echo 'Switching to user' ${USER}
