cb8f64350ea5e8a13418a39c69cff0d730ba5e61
Max Baak <Baak.Max@kpmg.nl>
2018-12-09 15:05:23 +0100

    Moved set_matplotlib_backend function to Eskapade-Core
    Reason: to break implicit dependency from Eskapade-Core on Eskapade.

--- python/eskapade/visualization/__init__.py
+++ python/eskapade/visualization/__init__.py
@@ -2,5 +2,5 @@
 from eskapade.visualization import vis_utils
 from eskapade.visualization.links import *
 
-from eskapade.visualization import config
-config.set_matplotlib_backend()
+from escore.utils import set_matplotlib_backend
+set_matplotlib_backend()
